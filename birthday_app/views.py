58f4c45c7ebd147504705aa9211b771f41970735
readywebsites <biz499websites@gmsil.com>
2025-08-05 10:20:30 +0530

    timer

--- birthday_app/views.py
+++ birthday_app/views.py
@@ -3,7 +3,7 @@ import datetime
 from django.utils import timezone as django_timezone
 
 # Static values for timer, letter, and paragraph
-STATIC_RELEASE_TIME = datetime.datetime(2025, 8, 5, 12, 0, 0, tzinfo=datetime.timezone.utc)
+STATIC_RELEASE_TIME = datetime.datetime(2025, 8, 5, 6, 30, 0, tzinfo=datetime.timezone.utc)
 STATIC_LETTER_TEXT = "You will get the Letter Tommorow!!"
 STATIC_PARAGRAPH_TEXT = "On this wonderful day, I wish you all the happiness and success in the world. May your day be filled with joy and laughter."
 
